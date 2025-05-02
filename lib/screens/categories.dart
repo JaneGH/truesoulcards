0aca6f200ab99f6f27a7d4d71b31a288d676f4a9
JaneGH <jkrytsyna@gmail.com>
2025-05-02 10:10:57 -0700

    fixed TabController

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -17,24 +17,27 @@ class CategoriesScreen extends ConsumerWidget {
 
   const CategoriesScreen({super.key, required this.mode});
 
-  Future<void> _selectCategory(BuildContext context,  Category category, bool isEdit, WidgetRef ref,) async {
+  Future<void> _selectCategory(
+    BuildContext context,
+    Category category,
+    bool isEdit,
+    WidgetRef ref,
+  ) async {
     if (isEdit) {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (ctx) =>
-              QuestionsScreen(
-                  title: category.title),
+          builder: (ctx) => QuestionsScreen(title: category.title),
         ),
       );
-    }else{
+    } else {
       // final question = await ref.read(firstQuestionInCategoryProvider(category.id).future);
       // if (question != null && context.mounted) {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (_) => QuestionSwiperScreen(category: category),
-          ),
-        );
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => QuestionSwiperScreen(category: category),
+        ),
+      );
       // }
     }
   }
@@ -43,31 +46,47 @@ class CategoriesScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final categoriesAsync = ref.watch(categoriesProvider);
     final isEdit = mode == ScreenMode.edit;
-    var appBarText ="Pick your category";
+    var appBarText = "Pick your category";
     if (isEdit) {
       appBarText = "Pick to edit";
     }
-    return Scaffold(
-      appBar: AppBar(title: Text(appBarText)),
-      body: categoriesAsync.when(
-        data: (availableCategories) {
-          final adultCategories = availableCategories.where((category) => category.subcategory == 'adults').toList();
-          final kidsCategories = availableCategories.where((category) => category.subcategory == 'kids').toList();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(appBarText),
+          bottom: const TabBar(tabs: [Tab(text: 'Adults'), Tab(text: 'Kids')]),
+        ),
+        body: categoriesAsync.when(
+          data: (availableCategories) {
+            final adultCategories =
+                availableCategories
+                    .where((category) => category.subcategory == 'adults')
+                    .toList();
+            final kidsCategories =
+                availableCategories
+                    .where((category) => category.subcategory == 'kids')
+                    .toList();
 
-          return TabBarView(
-            children: [
-              _buildCategoryGrid(adultCategories, isEdit, ref),
-              _buildCategoryGrid(kidsCategories, isEdit, ref),
-            ],
-          );
-        },
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (err, stack) => Center(child: Text('Error: $err')),
+            return TabBarView(
+              children: [
+                _buildCategoryGrid(adultCategories, isEdit, ref),
+                _buildCategoryGrid(kidsCategories, isEdit, ref),
+              ],
+            );
+          },
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (err, stack) => Center(child: Text('Error: $err')),
+        ),
       ),
     );
   }
 
-  Widget _buildCategoryGrid(List<Category> categories, bool isEdit, WidgetRef ref) {
+  Widget _buildCategoryGrid(
+    List<Category> categories,
+    bool isEdit,
+    WidgetRef ref,
+  ) {
     return GridView(
       padding: const EdgeInsets.all(20),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -87,6 +106,4 @@ class CategoriesScreen extends ConsumerWidget {
       ],
     );
   }
-
 }
-
